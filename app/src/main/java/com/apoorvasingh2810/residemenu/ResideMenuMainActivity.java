49e8d87034ee68802df5b52becc9fd1ef7de36a2
Apoorva Singh <apoorva.singh2810@gmail.com>
2018-07-17 17:55:09 +0530

    Project Added

---
+++ app/src/main/java/com/apoorvasingh2810/residemenu/ResideMenuMainActivity.java
@@ -0,0 +1,13 @@
+package com.apoorvasingh2810.residemenu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ResideMenuMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reside_menu_main);
+    }
+}
